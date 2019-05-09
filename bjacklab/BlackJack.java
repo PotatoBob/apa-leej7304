389045de1da3a1803ade895d5d3df10446e1c697
Jangwoo Lee <jangwoo24@gmail.com>
2019-05-08 20:22:04 -0700

    bust works
     if the player hits and goes over 21, then the game finishes. will change later once replays are implemented

--- bjacklab/BlackJack.java
+++ bjacklab/BlackJack.java
@@ -38,8 +38,23 @@ public class BlackJack
     dealer.addCardToHand(dealer.deal());
     dealer.addCardToHand(dealer.deal());
 
-    System.out.println("Player:\n" + player);
-    System.out.println("Dealer:\n" + dealer);
+    System.out.println("Player Hand:\n" + player);
+    //we don't know dealer's hand until after player finishes
+
+    while(player.getHandValue() <= 21 && hit) {
+    	System.out.print("hit? y/n :: ");
+    	if(hit = (keyboard.next().charAt(0) == 'y')) {
+    		player.addCardToHand(dealer.deal());
+    	} else {
+    		hit = false;
+    	}
+    	System.out.println("Player Hand:\n" + player);
+    }
+    if(player.getHandValue() > 21) {
+    	System.out.println("Shucks! You busted!");
+    	return;
+    }
+    //else dealer's turn
   }
         
   public static void main(String[] args)
